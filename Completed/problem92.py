5a6db53fc4b79e87f695a481f7299c9c8a974cb4
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-04-26 14:02:35 +0300

    Add problem 92

--- Completed/problem92.py
+++ Completed/problem92.py
@@ -3,41 +3,31 @@ from time import clock
 def square_digits(number):
     return sum([int(digit) ** 2 for digit in str(number)])
 
-def becomes_one(number):
-    global becomes_one_dict
+def becomes_1(number):
+    global sequence_to_89
+    global sequence_to_1
     original_number = number
-    keys = becomes_one_dict.keys()
+    traversed_nums = set()
 
     while True:
-        if number in keys:
-            # print("Using dict value")
-            became_one = becomes_one_dict[number]
-            becomes_one_dict[original_number] = became_one
-            return became_one
-        else:
-            # print("Value not found")
-            number = square_digits(number)
-
-        if number == 1:
-            becomes_one_dict[original_number] = True
-            return True
-        elif number == 89:
-            becomes_one_dict[original_number] = False
+        traversed_nums.add(number)
+        number = square_digits(number)
+        if number in sequence_to_89:
+            sequence_to_89.update(traversed_nums)
             return False
-
+        if number in sequence_to_1:
+            sequence_to_1.update(traversed_nums)
+            return True
 
 if __name__ == "__main__":
     clock()
 
-    global becomes_one_dict
-    becomes_one_dict = {}
+    global sequence_to_89; sequence_to_89 = {89}
+    global sequence_to_1; sequence_to_1 = {1}
 
-    number, count = 1, 0
-
-    while number < 10e6:
-        if not becomes_one(number):
-            count += 1
-        number += 1
+    count = 0
+    for number in range(1, int(10e6)):
+        if not becomes_1(number): count += 1
 
     print(count)
-    print("Took " + str(clock()))
+    print clock()
