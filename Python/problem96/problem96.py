7042b232295e11fbc0192982336b2f7cb81913ad
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-03 12:11:27 +0300

    tile_at_index passes tests

--- Python/problem96/problem96.py
+++ Python/problem96/problem96.py
@@ -2,16 +2,22 @@ grids_file = open('sudoku.txt')
 
 first_grid = [line.strip() for line in grids_file if 'Grid' not in line][0:9]
 
-# possibilites = set([str(i) for i in range(1, 10)])
-
 def line_at_index(index): return first_grid[index]
 
 def column_at_index(index): return ''.join([str(line[index]) for line in first_grid])
 
 def tile_at_index(index):
-    for i in range(0, 7, 3)
+    if index == 0 or index == 1 or index == 2:
+        lines = (0, 1, 2)
+    elif index == 3 or index == 4 or index == 5:
+        lines = (3, 4, 5)
+    elif index == 7 or index == 8 or index == 9:
+        lines = (6, 7, 8)
+    
+    columns = [i + (index % 3) * 3 for i in (0, 1, 2)]
 
-    [j for j in range(30) if i <= (j % 9) < i+3])
     tile = ''
-    for j in range(6):
-        triple = ''.join([num for num in first_grid[]])
+    for line in lines:
+        for column in columns:
+            tile += first_grid[line][column]
+    return tile
