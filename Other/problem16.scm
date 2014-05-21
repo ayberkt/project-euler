1873c3865c995cbbaea066fa51ca5dac3ab4ac6f
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-21 10:07:21 +0300

    Recursive, mathematical digit-summing function

--- Other/problem16.scm
+++ Other/problem16.scm
@@ -1,13 +1,14 @@
-;; MIT Scheme
+;;; MIT Scheme
 
-(define digits
-  (lambda (n)
-    (map string->number 
-      (map char->string 
-        (string->list (number->string n))))))
+(define sum-of-digits
+  (lambda (num sum)
+    (let ((next-digit (modulo num 10))
+          (remaining-num (quotient num 10)))
+      (if (> remaining-num 0)
+        (sum-of-digits remaining-num (+ sum next-digit))
+        sum))))
 
-(define sum-digits (lambda (n) (reduce + '() (digits n))))
 
 (let ((input (arithmetic-shift 2 999)))
-  (sum-digits input))
+  (sum-of-digits input 1))
 
