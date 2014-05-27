918d1099f2059615393f23f69fe959245e1a5e04
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-27 16:24:05 +0300

    Minor refactoring

--- python/problem53.py
+++ python/problem53.py
@@ -5,12 +5,11 @@ def nCr(n, r):
     combination = factorial(n) / (factorial(r) * factorial(n - r))
     return combination
 
-scope = range(1, 100)
-counter = 0
+if __name__ == '__main__':
+    count = 0
+    for i in range(1, 101):
+        for j in range(1, i + 1):
+            if nCr(i, j) > 1000000:
+                count += 1
 
-for i in range(1, 101):
-    for j in range(1, i + 1):
-        if nCr(i, j) > 1000000:
-            counter += 1
-
-print counter
+    print(count)
