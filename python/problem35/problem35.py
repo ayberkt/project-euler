2587b2e9b15df84576a064afafe0e48ddca72de8
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-31 10:54:43 +0300

    Reduce time by memoization

--- python/problem35/problem35.py
+++ python/problem35/problem35.py
@@ -1,5 +1,32 @@
+from math import sqrt
 
 
+def memoize(f):
+    cache = {}
+
+    def helper(x):
+        if x not in cache:
+            cache[x] = f(x)
+        return cache[x]
+
+    return helper
+
+
+@memoize
+def is_prime(n):
+    if n == 2:
+        return True
+    if n == 1:
+        return False
+
+    test_nums = range(2, int(sqrt(n)) + 2)
+    for i in test_nums:
+        if n % i == 0:
+            return False
+    return True
+
+
+@memoize
 def rotations(num):
     rotations = set()
     digit_array = list(str(num))
@@ -9,3 +36,14 @@ def rotations(num):
         rotations.add(int(''.join(digit_array)))
 
     return list(rotations)
+
+
+def rotational_prime(num):
+    return all([is_prime(n) for n in rotations(num)])
+
+
+if __name__ == '__main__':
+    rotational_primes = [2] + [n for n in range(1, 1000000, 2)
+                               if rotational_prime(n)]
+    print(rotational_primes)
+    print(len(rotational_primes))
