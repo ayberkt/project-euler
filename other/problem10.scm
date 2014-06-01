073ee02e0cbdd6bccb88a7a122611bf734dce882
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-06-01 22:10:52 +0300

    Add problem 10 in scheme

---
+++ other/problem10.scm
@@ -0,0 +1,23 @@
+(define prime?
+  (case-lambda
+    [(x)
+     (prime? x (cddr  (iota (inexact->exact (round (add1 (sqrt x)))))))]
+    [(x test-seq)
+     (cond
+       ((null? test-seq) #t)
+       (else (cond
+               ((zero? (modulo x (car test-seq))) #f)
+                (else (prime? x (cdr test-seq))))))]))
+(define memoize
+  (lambda (proc)
+    (let ([cache '()])
+      (lambda (x)
+        (cond
+          [(assq x cache) => cdr]
+          [else
+           (let ([ans (proc x)])
+             (set! cache (cons (cons x ans) cache))
+             ans)]))))) 
+
+
+(display (fold-left + 0 (filter prime? (cddr (iota 2000000)))))
