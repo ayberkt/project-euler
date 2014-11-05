e2e04e71208b32fa666fdb253973b048de027380
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-11-04 20:57:30 -0600

    Minor improvement

--- other/problem6.hs
+++ other/problem6.hs
@@ -2,6 +2,4 @@ sumSquares x = sum $ map (^2) [1..x]
 
 squareSum x = ((x * (x + 1)) / 2)^2
 
-difference x = squareSum x - sumSquares x
-
-main = print $ difference 100
+main = print $  (\x -> squareSum x - sumSquares x) 100
