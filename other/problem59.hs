1cd8d8e294a09cecf4a39eb0cdcc6aa988e2f80d
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-06-19 17:30:24 +0300

    Type alias

--- other/problem59.hs
+++ other/problem59.hs
@@ -3,17 +3,19 @@ import Data.Bits (xor)
 import Data.List.Split (splitOn)
 import Data.List
 
+type Cipher = [Int]
+
 extendKey :: String -> Int -> String
 extendKey key messageLength = concat $ (replicate quotient key) ++ [(take remainder key)]
   where quotient = messageLength `div` length key
         remainder = messageLength `mod` length key
 
 
-decrypt :: [Int] -> String -> String
-decrypt crypt key = map chr $ zipWith xor crypt $
-                                      map ord $ extendKey key $ length crypt
+decrypt :: Cipher -> String -> String
+decrypt cipher key = map chr $ zipWith xor cipher $
+                                       map ord $ extendKey key $ length cipher
 
-possibleDecryptions :: [Int] -> [String]
+possibleDecryptions :: Cipher -> [String]
 possibleDecryptions message = map (decrypt message) keys
   where keys = [[a,b,c] | a <- ['a'..'z'], b <- ['a'..'z'], c <- ['a'..'z']]
 
@@ -21,4 +23,5 @@ main = do
   contents <- readFile "cipher.txt"
   let cipher = map (\n -> read n :: Int) $ splitOn "," contents
       decryptedMessage = head $ filter (isInfixOf " and ") $ possibleDecryptions cipher
-  putStrLn $ "ASCII sum is " ++ show (sum $ map ord decryptedMessage)
+  putStrLn decryptedMessage
+  -- putStrLn $ "ASCII sum is " ++ show (sum $ map ord decryptedMessage)
