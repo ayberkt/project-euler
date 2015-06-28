27cda9fc691d6bb099eda3be8edf50c999f5cbf8
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-06-29 01:12:03 +0300

    Add explanation for problem 67

--- other/problem67.hs
+++ other/problem67.hs
@@ -1,6 +1,12 @@
 import Data.List (elemIndex, sort)
 import Data.Char (digitToInt)
 
+-- | This is *exactly* the same as problem 18, with the only
+-- | difference being the name of the file from which the
+-- | triangle is loaded. The reason I copied and pasted the
+-- | solution for problem 18 is that I don't maintain a module
+-- | system in Project Euler solutions.
+
 assessNext :: [Int] -> [Int] -> [Int]
 assessNext xs ys = [(xs !! i) + if (ys !! i) > (ys !! (i+1))
                                 then (ys !! i)
@@ -11,7 +17,7 @@ pathWeights (x1:x2:xs)
   | otherwise = x1 : (pathWeights $ (assessNext x2 x1):xs)
 
 main = do
-  triangle <- readFile "problem18-triangle.txt"
+  triangle <- readFile "problem67-triangle.txt"
   let rows = lines triangle
       numStrs = map words rows
       stringToInt :: [Char] -> Int
